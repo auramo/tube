f58a1d0b253bb785ea6ff30b1dfb943fbce79dc8
Perttu Auramo <perttu.auramo@gmail.com>
2016-02-25 20:40:44 +0200

    Map parameters to handlers
    Example value coming through

--- front/src/core.cljs
+++ front/src/core.cljs
@@ -5,8 +5,15 @@
 
 (enable-console-print!)
 
+(defn handle-response [state response]
+  (println "state: " @state)
+  (println "response: " response)
+  ;;  (reset! state (js->clj response :keywordize-keys true))
+  (reset! state (clojure.walk/keywordize-keys response))
+  (println "state after reset: " @state))
+
 (defn fetch-state [state]
-  (GET "/api/radiator-state" {:handler #(println "response" %)
+  (GET "/api/radiator-state" {:handler #(handle-response state %) ;;#(handle-response state %)
                                 :error-handler #(println "error" %)}))
 
 (defn app-fn [state]
@@ -15,7 +22,7 @@
 (defn app-m [state] (with-meta #(app-fn state) {:component-did-mount #(fetch-state state) }))
 
 (defn init []
-  (let [state (atom {:title "Tube"})]
+  (let [state (reagent/atom {:title "Tube"})]
     (reagent/render [(app-m state)] (.getElementById js/document "content"))))
 
 (init)
--- front/src/radiator_display.cljs
+++ front/src/radiator_display.cljs
@@ -12,19 +12,25 @@
        [:div.parameter-name (:title parameter)]]
       [[:div.parameter "N/A"]])))
 
+(defn unknown-type [] [:div.parameter-box.gloss [:div.parameter "??"]])
+
 (defn simple-parameter [parameter]
   (into [:div.parameter-box.gloss] (simple-parameter-contents parameter)))
-          ;; [:div.warning (str "Updated: " (millis->datestr (:updated parameter)))]
-          ;; [:div.parameter (:value parameter)]
-          ;; [:div.parameter-name (:title parameter)]]))
+
+(defn unknown-type [] [:div.parameter-box.gloss [:div.parameter "??"]])
+
+(def param-mappers {"simple" simple-parameter})
+
+(defn map-parameters [parameters]
+  (if parameters
+    (map (fn [param]
+           (if-let [mapper (get param-mappers (:type param))]
+             (mapper param)
+             (unknown-type)))
+           parameters)
+    [[:div.warning "No parameters available"]]))
 
 (defn radiator [state]
   [:div
    [:div.header (str (:title @state) " radiator")]
-   [:div.parameters
-    (simple-parameter {:id "orders" :title "Orders" :type "simple" :value 44 :updated 1456339348361})
-    ;;[:div.parameter-box.gloss
-    ;; [:div.warning "Delayed"]
-    ;; [:div.parameter 77]
-    ;; [:div.parameter-name "Tilaukset"]]
-    ]])
+   (into [:div.parameters] (map-parameters (:parameters @state)))])
